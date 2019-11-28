19799d0b239a78af1822de60bc28c736b1277c2d
AT-Seon <keangsiang.pua@armonia-tech.com>
2019-11-28 09:54:26 +0800

    add headless main file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# armonia-headless
+This is Armonia Tech project.
+===================================================================
+
+This project is to headless library.
+ 
+Installation: 
+
+`composer require armonia-tech/armonia-headless`
---
+++ src/Headless.php
@@ -0,0 +1,55 @@
+<?php
+namespace ArmoniaHeadless;
+
+use HeadlessChromium\BrowserFactory;
+
+class Headless
+{
+    private $browser;
+    private $page;
+
+    /**
+     * Construct
+     *
+     * @author Seon <keangsiang.pua@armonia-tech.com>
+     * @param  string $url
+     * @param  array  $settings
+     * @return void
+     */
+    public function __construct(string $url, array $settings = ['noSandbox' => true])
+    {
+        $browserFactory = new BrowserFactory('chromium-browser');
+
+        // starts headless chrome
+        $this->browser = $browserFactory->createBrowser($settings);
+
+        // creates a new page and navigate to an url
+        $this->page = $browser->createPage();
+        $this->page->navigate($url)->waitForNavigation();
+        $this->page->addScriptTag([
+            'url' => 'https://code.jquery.com/jquery-3.4.1.min.js'
+        ])->waitForResponse();
+    }
+
+    /**
+     * Get Page
+     *
+     * @author Seon <keangsiang.pua@armonia-tech.com>
+     * @return object
+     */
+    public function getBrowser()
+    {
+        return $this->browser;
+    }
+
+    /**
+     * Get Page
+     *
+     * @author Seon <keangsiang.pua@armonia-tech.com>
+     * @return object
+     */
+    public function getPage()
+    {
+        return $this->page;
+    }
+}
