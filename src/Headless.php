c140718fa31907f6faabe4be748fcdbbba558b4f
AT-Wilson <huanyong.chan@armonia-tech.com>
2020-12-10 15:46:08 +0800

    MDB-209: add pre_script

--- src/Headless.php
+++ src/Headless.php
@@ -14,9 +14,10 @@ class Headless
      * @author Seon <keangsiang.pua@armonia-tech.com>
      * @param  string $url
      * @param  array  $settings
+     * @param  string $pre_script default null
      * @return void
      */
-    public function __construct(string $url, array $settings = ['noSandbox' => true])
+    public function __construct(string $url, array $settings = ['noSandbox' => true], string $pre_script = null)
     {
         $browserFactory = new BrowserFactory('chromium-browser');
 
@@ -25,6 +26,11 @@ class Headless
 
         // creates a new page and navigate to an url
         $this->page = $this->browser->createPage();
+
+        if (isset($pre_script)) {
+            $this->page->addPreScript($pre_script);
+        }
+
         $this->page->navigate($url)->waitForNavigation();
         $this->page->addScriptTag([
             'url' => 'https://code.jquery.com/jquery-3.4.1.min.js'
